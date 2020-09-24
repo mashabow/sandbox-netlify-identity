a48ced814efbe96d421d458cf92ffe8c1fea1651
Masaya Nakamura <mashabow@shiromoji.net>
2020-09-25 01:46:17 +0900

    a

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,26 @@
-import React from 'react';
-import useNetlifyIdentity from './useNetlifyIdentity';
-import './App.css';
+import React from "react";
+import useNetlifyIdentity from "./useNetlifyIdentity";
 
 const App: React.FC = () => {
-  const { user, openLoginModal } = useNetlifyIdentity();
+  const { user, openModal, logout } = useNetlifyIdentity();
   return (
-    <div className="App">
-      {user ? (
-        <code>
-          {user}
-        </code>
-      ) : (
-        <button onClick={openLoginModal}>
-          Log in
-        </button>
+    <div style={{ margin: 40 }}>
+      <p>
+        {user ? (
+          <button onClick={logout}>Log out</button>
+        ) : (
+          <button onClick={openModal}>Log in</button>
+        )}
+      </p>
+      {user && (
+        <textarea
+          value={JSON.stringify(user, null, 2)}
+          readOnly
+          style={{ width: "100%", height: 500 }}
+        />
       )}
     </div>
   );
-}
+};
 
 export default App;
--- src/useNetlifyIdentity.ts
+++ src/useNetlifyIdentity.ts
@@ -1,21 +1,22 @@
 import { useEffect, useState } from "react";
 import * as netlifyIdentity from "netlify-identity-widget";
 
-
-netlifyIdentity.on("init", (user) => console.log("init", user));
 netlifyIdentity.init();
 
 const useNetlifyIdentity = () => {
-  const [user, setUser] = useState<netlifyIdentity.User>();
+  const [user, setUser] = useState<netlifyIdentity.User | null>(
+    netlifyIdentity.currentUser()
+  );
 
   useEffect(() => {
     netlifyIdentity.on("login", (user) => setUser(user));
-    netlifyIdentity.on("logout", () => setUser(undefined));
+    netlifyIdentity.on("logout", () => setUser(null));
   }, []);
 
   return {
     user,
-    openLoginModal: () => netlifyIdentity.open(),
+    openModal: () => netlifyIdentity.open(),
+    logout: () => netlifyIdentity.logout(),
   };
 };
 
